3477af52562a0fa0304270eae61a3beb28c319e8
Tony Bowden <tony@tmtm.com>
2015-08-12 17:33:55 +0100

    Initial scraper

---
+++ scraper.rb
@@ -0,0 +1,31 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'json'
+require 'pry'
+require 'rest-client'
+require 'scraperwiki'
+require 'wikidata/fetcher'
+require 'mediawiki_api'
+
+def candidates
+  morph_api_url = 'https://api.morph.io/tmtmtmtm/british-virgin-islands-assembly-wikipedia/data.json'
+  morph_api_key = ENV["MORPH_API_KEY"]
+  result = RestClient.get morph_api_url, params: {
+    key: morph_api_key,
+    query: "select DISTINCT(wikipedia__en) as wikiname from data"
+  }
+  JSON.parse(result, symbolize_names: true)
+end
+
+WikiData.ids_from_pages('en', candidates.map { |c| c[:wikiname] }).each_with_index do |p, i|
+  puts i if (i % 50).zero?
+  # next if ScraperWiki.select('COUNT(*) as gotit FROM data WHERE id = ?', p.last).first["gotit"] == 1
+  data = WikiData::Fetcher.new(id: p.last).data('en') rescue nil
+  unless data
+    warn "No data for #{p}"
+    next
+  end
+  ScraperWiki.save_sqlite([:id], data)
+end
+
